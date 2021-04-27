41e6016ebf0f7f0afe1a1749f95dd8e41aba1afd
Aditi Kanaujia <akanuajia8@gmail.com>
2021-04-27 00:17:40 -0500

    added player movement script

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody2D rb;
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+    bool grounded = true;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+        moveVelocity = 0;
+
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        rb.velocity = new Vector2(moveVelocty.rb.velocty.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Grounded")
+            grounded = true;
+    }
+    private void OnTriggerEnterExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Grounded")
+            grounded = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
