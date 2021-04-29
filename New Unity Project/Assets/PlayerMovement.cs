f9f38191af38c465c09e0c3ee1808e409305b97b
Aditi Kanaujia <akanuajia8@gmail.com>
2021-04-29 00:01:03 -0500

    added player animations and fixed details

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -10,30 +10,38 @@ public class PlayerMovement : MonoBehaviour
     float moveVelocity;
 
     bool grounded = true;
-
+    public Animator Anim;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (grounded)
                 rb.velocity = new Vector2(rb.velocity.x, jump);
+            Anim.SetInteger("Anim", 2);
         }
         moveVelocity = 0;
 
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             moveVelocity = -speed;
+            this.gameObject.transform.localScale = new Vector2(-1, 1);
+            Anim.SetInteger("Anim", 1);
         }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.A))
         {
             moveVelocity = speed;
+            this.gameObject.transform.localScale = new Vector2(1, 1);
+            Anim.SetInteger("Anim", 1);
         }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        Anim.SetInteger("Anim", 0);
         rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Grounded")
             grounded = true;
+        Anim.SetInteger("Anim", 0);
     }
     private void OnTriggerEnterExit2D(Collider2D collision)
     {
